9f7d237b212d6dcf1da98b3140a915b999f324ef
Brennan Shea <bshea@iMac.local>
2025-12-31 20:50:36 -0500

    Add scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import Layout from './components/Layout'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import PracticeAreas from './pages/PracticeAreas'
 import WorkersComp from './pages/WorkersComp'
@@ -16,23 +17,27 @@ function App() {
     const location = useLocation()
 
     return (
-        <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="practice-areas" element={<PracticeAreas />} />
-                    <Route path="workers-comp" element={<WorkersComp />} />
-                    <Route path="longshore-comp" element={<LongshoreComp />} />
-                    <Route path="personal-injury" element={<PersonalInjury />} />
-                    <Route path="ss-disability" element={<SSDisability />} />
-                    <Route path="mediation" element={<Mediation />} />
-                    <Route path="attorneys" element={<Attorneys />} />
-                    <Route path="testimonials" element={<Testimonials />} />
-                    <Route path="contact" element={<Contact />} />
-                </Route>
-            </Routes>
-        </AnimatePresence>
+        <>
+            <ScrollToTop />
+            <AnimatePresence mode="wait">
+                <Routes location={location} key={location.pathname}>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="practice-areas" element={<PracticeAreas />} />
+                        <Route path="workers-comp" element={<WorkersComp />} />
+                        <Route path="longshore-comp" element={<LongshoreComp />} />
+                        <Route path="personal-injury" element={<PersonalInjury />} />
+                        <Route path="ss-disability" element={<SSDisability />} />
+                        <Route path="mediation" element={<Mediation />} />
+                        <Route path="attorneys" element={<Attorneys />} />
+                        <Route path="testimonials" element={<Testimonials />} />
+                        <Route path="contact" element={<Contact />} />
+                    </Route>
+                </Routes>
+            </AnimatePresence>
+        </>
     )
 }
 
 export default App
+
